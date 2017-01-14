100e0bd053bcf2567ddf685b97014fb1197692df
b1014100 <b1014100@fun.ac.jp>
2017-01-14 21:23:27 +0900

    Add Class Book

---
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/Book.java
@@ -0,0 +1,43 @@
+package com.google.android.gms.samples.vision.barcodereader;
+
+/**
+ * Created by b1014100_2 on 2017/01/14.
+ */
+
+public class Book {
+
+    int Bookid;
+    String name;
+    float locat;
+
+    /*-----constract---------------*/
+    public Book(int Bookid, String name, float locat){
+        this.Bookid = Bookid;
+        this.name = name;
+        this.locat  = locat;
+    }
+
+   /*-----------getter-------------*/
+    public String getName() {
+        return name;
+    }
+    public float getLocat() {
+        return locat;
+    }
+
+    public int getBookid() {
+        return Bookid;
+    }
+
+/*-------------setter---------------*/
+    public void setBookid(int bookid) {
+        Bookid = bookid;
+    }
+    public void setLocat(float locat) {
+        this.locat = locat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
